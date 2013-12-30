bf7cc9cbc26ddf66300c33675e5dc578a3cabbd4
Lars Greiss <kufikugel@googlemail.com>
2013-12-30 15:44:14 +0700

    [1/3] Base: QuietHours (slimRoms) (Notification light doesn't work for now... )
    add support quietHours
    
    Squash Commit from:
    
     fb: Quit hours 1/2 thanks cm
    
    and added multiuser support
    
    Change-Id: I38e8560297099629aed94ee001da7f1ebaa04dac
    
    by Kufikugel
    
     Frameworks: Quiet Hours Change Logic (1/2)
    
    12:00am - 12:00am = 24 hours
    	(Or any "==" time)
    
    This makes more sense for the user
    	if they have the direct ability
    	to toggle quiet hours.
    	The feature should work as-is
    	and in a 24-hour on/off manner
    	through using the toggle.
    
    Change-Id: Ib10ab5c1ac9278ab1f195d48f88de7357722979c
    
    by Jubakuba
    
     fb: refactor QuietHours code
    
    thanks to ivisiblk for the idea
    
    Change-Id: I56e2b4a1945a4b93251b9937b701c86168b732b4
    
    by Kufikugel
    
     Quiet Hours Ringer (2/3)
    
    Change-Id: Id58ffb24f0d0d57d8446265c899ec4124fe00c70
    
    by Jubakuba
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Change-Id: Icce9e3a804ab8be753b8a23a28668ef1358540b1
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	services/java/com/android/server/BatteryService.java
    	services/java/com/android/server/NotificationManagerService.java

--- services/java/com/android/server/NotificationManagerService.java
+++ services/java/com/android/server/NotificationManagerService.java
@@ -81,6 +81,7 @@ import android.widget.Toast;
 import com.android.internal.R;
 
 import com.android.internal.notification.NotificationScorer;
+import com.android.internal.util.slim.QuietHoursHelper;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
@@ -1279,10 +1280,23 @@ public class NotificationManagerService extends INotificationManager.Stub
                     Settings.System.NOTIFICATION_LIGHT_PULSE_CUSTOM_ENABLE), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.NOTIFICATION_LIGHT_PULSE_CUSTOM_VALUES), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QUIET_HOURS_ENABLED), false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QUIET_HOURS_START), false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QUIET_HOURS_END), false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QUIET_HOURS_MUTE), false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QUIET_HOURS_STILL), false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QUIET_HOURS_DIM), false, this, UserHandle.USER_ALL);
             update(null);
         }
 
-        @Override public void onChange(boolean selfChange, Uri uri) {
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
             update(uri);
             updateNotificationPulse();
         }
@@ -1912,14 +1926,17 @@ public class NotificationManagerService extends INotificationManager.Stub
                         Uri soundUri = null;
                         boolean hasValidSound = false;
 
-                        if (useDefaultSound) {
+                        if (!(QuietHoursHelper.inQuietHours(
+                                    mContext, Settings.System.QUIET_HOURS_MUTE))
+                                && useDefaultSound) {
                             soundUri = Settings.System.DEFAULT_NOTIFICATION_URI;
 
                             // check to see if the default notification sound is silent
                             ContentResolver resolver = mContext.getContentResolver();
                             hasValidSound = Settings.System.getString(resolver,
                                    Settings.System.NOTIFICATION_SOUND) != null;
-                        } else if (notification.sound != null) {
+                        } else if (!(QuietHoursHelper.inQuietHours(mContext,
+                                Settings.System.QUIET_HOURS_MUTE)) && notification.sound != null) {
                             soundUri = notification.sound;
                             hasValidSound = (soundUri != null);
                         }
@@ -1966,7 +1983,9 @@ public class NotificationManagerService extends INotificationManager.Stub
                         final boolean useDefaultVibrate =
                                 (notification.defaults & Notification.DEFAULT_VIBRATE) != 0;
 
-                        if ((useDefaultVibrate || convertSoundToVibration || hasCustomVibrate)
+                        if (!(QuietHoursHelper.inQuietHours(
+                                    mContext, Settings.System.QUIET_HOURS_STILL))
+                                && (useDefaultVibrate || convertSoundToVibration || hasCustomVibrate)
                                 && !(audioManager.getRingerMode()
                                         == AudioManager.RINGER_MODE_SILENT)) {
                             mVibrateNotification = r;
@@ -2292,9 +2311,10 @@ public class NotificationManagerService extends INotificationManager.Stub
         }
 
         // Don't flash while we are in a call or screen is on
-        if (mLedNotification == null || mInCall || mScreenOn) {
+        if (mLedNotification == null || mInCall || mScreenOn || !mNotificationPulseEnabled ||
+            (QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_DIM))) {
             mNotificationLight.turnOff();
-        } else {
+        } else if (mNotificationPulseEnabled) {
             final Notification ledno = mLedNotification.sbn.getNotification();
             final NotificationLedValues ledValues = getLedValuesForNotification(mLedNotification);
             int ledARGB = ledno.ledARGB;
@@ -2470,3 +2490,4 @@ public class NotificationManagerService extends INotificationManager.Stub
         }
     }
 }
+
