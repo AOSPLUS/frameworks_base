5bf5ab3d8f2624d2e2c9fed4e630f5fcfd02b9a1
Javierd <Javierd299@gmail.com>
2014-01-25 19:44:06 +0100

    Revert "Revert "Lockscreen: See through (1/2)""
    This reverts commit 9d37accc4eef23b8267d238eb553cbdd93251cff.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2006 The Android Open Source Project
+ * This code has been modified. Portions copyright (C) 2013, ParanoidAndroid Project.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -2853,6 +2854,12 @@ public final class Settings {
          * @hide
          */
         public static final String BATTERY_AROUND_LOCKSCREEN_RING = "battery_around_lockscreen_ring";
+
+        /**
+         * Allows to show the background activity back the lockscreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_SEE_THROUGH = "lockscreen_see_through";
 
         /**
          * Volume rocker wake
